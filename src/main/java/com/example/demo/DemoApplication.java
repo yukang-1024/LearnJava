914b9dcdb2a7a22745254e5b07f0b32cb55d3ad1
yukang <yukangkang@r7tec.com>
2020-07-29 16:24:11 +0800

    修改

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yukang
+ * @time 2020/07/28
+ * @desc  springBoot启动类
+ */
 @SpringBootApplication
 public class DemoApplication {
 
